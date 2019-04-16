64904ac74b05ce1a9b97fdd57e342fd3bd78ff4c
Christian <chris_1693@yahoo.com>
2019-04-16 16:36:10 -0400

    Update image.js
    -statement - resets input value
    -creates images and adds a click event

--- image.js
+++ image.js
@@ -8,3 +8,16 @@ window.onload = function () {
         // Gets image input and its value
         var imageInput = document.getElementById('image-url');
         var imageURL = imageInput.value;
+
+        if (isURL(imageURL)) {
+            // Resets input value
+            imageInput.value = '';
+
+            // Creates image and adds a click event listener
+            var image = document.createElement('img');
+            image.src = imageURL;
+            image.onclick = function () {
+                imageInput.value = imageURL;
+            };
+
+
