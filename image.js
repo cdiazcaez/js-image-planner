17165a20a6dd2b15374690eeaa658a728b021cac
Christian <chris_1693@yahoo.com>
2019-04-16 16:32:58 -0400

    image.js
    creating window.onload function.
    
    -The window object in JS has an event handler called onload. When this event handler is used, the entire page and all of its related files and components are loaded before the function listed in the onload event handler is executed.

---
+++ image.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+
+    // Add button click
+    document.getElementById('add').onclick = function () {
+        // Cleans any previous error message
+        cleanError();
