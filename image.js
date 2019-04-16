200035f49a89ea4aa3be39737598c7c53d009725
Christian <chris_1693@yahoo.com>
2019-04-16 17:19:23 -0400

    update image.js
    fixing JSlink errors "Use double quotes, not single quotes"

--- image.js
+++ image.js
@@ -1,49 +1,49 @@
 window.onload = function () {
 
     // Add button click
-    document.getElementById('add').onclick = function () {
+    document.getElementById("add").onclick = function () {
         // Cleans any previous error message
         cleanError();
 
         // Gets image input and its value
-        var imageInput = document.getElementById('image-url');
+        var imageInput = document.getElementById("image-url");
         var imageURL = imageInput.value;
 
         if (isURL(imageURL)) {
             // Resets input value
-            imageInput.value = '';
+            imageInput.value = "";
 
             // Creates image and adds a click event listener
-            var image = document.createElement('img');
+            var image = document.createElement("img");
             image.src = imageURL;
             image.onclick = function () {
                 imageInput.value = imageURL;
             };
             // Gets planning area and remove initial dots
-            var planningArea = document.getElementById('planning-area');
+            var planningArea = document.getElementById("planning-area");
 
-            if (planningArea.innerHTML === '...') {
-                planningArea.innerHTML = '';
+            if (planningArea.innerHTML === "...") {
+                planningArea.innerHTML = "";
             }
 
             // Adds image
             planningArea.appendChild(image);
         } else {
-            showError('Image URL is not valid');
+            showError("Image URL is not valid");
         }
     };
     // Delete button click
-    document.getElementById('delete').onclick = function () {
+    document.getElementById("delete").onclick = function () {
         // Cleans any previous error message
         cleanError();
 
         // Gets image input and its value
-        var imageInput = document.getElementById('image-url');
+        var imageInput = document.getElementById("image-url");
         var imageToRemove = imageInput.value;
         var found = false;
 
         // Gets all images
-        var images = document.getElementsByTagName('img');
+        var images = document.getElementsByTagName("img");
 
         // Loops over all images to remove the ones with the selected URL
         for (var i = 0; i < images.length; i++) {
@@ -59,25 +59,25 @@ window.onload = function () {
 
         if (found) {
             // Gets planning area; if it is empty, put some dots
-            var planningArea = document.getElementById('planning-area');
+            var planningArea = document.getElementById("planning-area");
 
-            if (planningArea.innerHTML === '') {
-                planningArea.innerHTML = '...';
+            if (planningArea.innerHTML === "") {
+                planningArea.innerHTML = "...";
             }
         } else {
-            showError('Image to remove not found');
+            showError("Image to remove not found");
         }
 
         // Resets image input
-        imageInput.value = '';
+        imageInput.value = "";
     };
 
 
     function showError(error) {
-        document.getElementById('error-message').innerHTML = error;
+        document.getElementById("error-message").innerHTML = error;
     }
     function cleanError() {
-        document.getElementById('error-message').innerHTML = '';
+        document.getElementById("error-message").innerHTML = "";
     }
 
     function isURL(str) {
